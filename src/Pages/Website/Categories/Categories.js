9c928016ecda15b798adcca2d5a843ef1ea2be93
Nouran <nouranhossam238@gmail.com>
2025-03-27 23:10:32 +0200

    edit

--- src/Pages/Website/Categories/Categories.js
+++ src/Pages/Website/Categories/Categories.js
@@ -6,7 +6,7 @@ import SideBar from "../Components/SideBar/SideBar";
 
 export default function Categories() {
   const [showSidebar, setShowSidebar] = useState(false);
-  let screen = false;
+  let screen = true;
   let categories = true;
 
   const toggleSidebar = () => {
